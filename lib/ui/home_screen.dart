1159f0ebcf42647db03f8d619c23ebd5f05260ec
mohamedshabaan223 <muhmddshabaan3@gmail.com>
2025-12-09 09:24:06 +0200

    finish logic home screen

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -14,6 +14,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool isMale = true;
+  double height = 0.0;
+  int weight = 30;
+  int age = 15;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,34 +51,73 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
             SizedBox(height: 10),
-            CustomHeight(),
+            CustomHeight(heihgt: height,
+            onChanged: (value) {
+              setState(() {
+                height = value;
+              });
+              
+            },),
             SizedBox(height: 10),
             Row(
               children: [
-                CustomCounter(label: 'Weight', value: 60),
+                CustomCounter(label: 'Weight',
+                 value: weight,
+                 decrement: () {
+                   if(weight > 1 ){
+                   setState(() {
+              weight--;       
+              
+                   });
+                   }
+                 },
+                 increment: () {
+                  setState(() {
+                     weight++;
+                  });
+                 },
+                 ),
                 SizedBox(width: 15),
-                CustomCounter(label: 'Age', value: 30),
+                CustomCounter(label: 'Age',
+                 value: age,
+                 increment: () {
+                   setState(() {
+                     age++;
+                   });
+                 },
+                 decrement: () {
+                   if (age > 1) {
+                    setState(() {
+                      age--;
+                    });
+                   }
+                 },),
               ],
             ),
           ],
         ),
       ),
-      bottomNavigationBar: Container(
-        alignment: Alignment.center,
-        height: 55,
-        decoration: BoxDecoration(
-          color: ColorsManger.pink,
-          borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(12),
-            bottomLeft: Radius.circular(12),
+      bottomNavigationBar: InkWell(
+        onTap: () {
+          
+        },
+        child: Container(
+          alignment: Alignment.center,
+          height: 55,
+          decoration: BoxDecoration(
+            color: ColorsManger.pink,
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(12),
+              bottomLeft: Radius.circular(12),
+            ),
           ),
-        ),
-        child: Text(
-          'Calculate',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.w600,
-            color: ColorsManger.white,
+          child: Text(
+            'Calculate',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w600,
+              color: ColorsManger.white,
+            ),
           ),
         ),
       ),
